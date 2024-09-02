19477a8a80b3a2ffdf238d6b5b6348547f8f203b
weipc <755197142@qq.com>
2024-09-02 14:35:43 +0800

    fix: 优化插件在多窗口下能被激活

--- src/extension.ts
+++ src/extension.ts
@@ -4,78 +4,79 @@ import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
   const workspaceFolders = vscode.workspace.workspaceFolders;
+  console.log("Congratulations, your extension 'angular-nestjs' is now active!");
   if (workspaceFolders) {
     const rootPath = workspaceFolders[0].uri.fsPath;
     const angularConfigPath = path.join(rootPath, "angular.json");
     const nestConfigPath1 = path.join(rootPath, "nest-cli.json");
-    const nestConfigPath2 = path.join(rootPath, ".nest-cli.json");
-    const nestConfigPath3 = path.join(rootPath, "nestconfig.json");
-    const nestConfigPath4 = path.join(rootPath, ".nestconfig.json");
 
-    if (fs.existsSync(angularConfigPath)) {
-      updateIconPath('angular');
-    } else if (
-      fs.existsSync(nestConfigPath1) ||
-      fs.existsSync(nestConfigPath2) ||
-      fs.existsSync(nestConfigPath3) ||
-      fs.existsSync(nestConfigPath4)
-    ) {
-      updateIconPath('nestjs');
+    if (!fs.existsSync(angularConfigPath)) {
+      return updateIconPath("angular");
+    } else if (!fs.existsSync(nestConfigPath1)) {
+      return updateIconPath("nestjs");
     }
   }
 }
 
-export function deactivate() {}
+export function deactivate() {
+  updateIconPath("nestjs");
 
+  return undefined;
+}
 
-function getFileExtensions(type: 'angular' | 'nestjs') {
-  if (type === 'angular') {
+function getFileExtensions(type: "angular" | "nestjs") {
+  if (type === "angular") {
     return {
-      'controller.ts': 'file_angularcontroller',
-      'controller.spec.ts': 'file_angularcontrollertest',
-      'controller.js': 'file_angularcontroller',
-      'controller.spec.js': 'file_angularcontrollertest',
-      'guard.ts': 'file_angularguard',
-      'guard.js': 'file_angularguard',
-      'module.js': 'file_angularmodule',
-      'module.ts': 'file_angularmodule',
-      'service.js': 'file_angularservice',
-      'service.spec.js': 'file_angularservicetest',
-      'service.ts': 'file_angularservice',
-      'service.spec.ts': 'file_angularservicetest',
-      'pipe.ts': 'file_angularpipe',
-      'pipe.js': 'file_angularpipe'
+      "controller.ts": "file_angularcontroller",
+      "controller.spec.ts": "file_angularcontrollertest",
+      "controller.js": "file_angularcontroller",
+      "controller.spec.js": "file_angularcontrollertest",
+      "guard.ts": "file_angularguard",
+      "guard.js": "file_angularguard",
+      "module.js": "file_angularmodule",
+      "module.ts": "file_angularmodule",
+      "service.js": "file_angularservice",
+      "service.spec.js": "file_angularservicetest",
+      "service.ts": "file_angularservice",
+      "service.spec.ts": "file_angularservicetest",
+      "pipe.ts": "file_angularpipe",
+      "pipe.js": "file_angularpipe",
     };
   } else {
     return {
-      'controller.ts': 'file_nestcontroller',
-      'controller.spec.ts': 'file_nestcontrollertest',
-      'controller.js': 'file_nestcontroller',
-      'controller.spec.js': 'file_nestcontrollertest',
-      'guard.ts': 'file_nestguard',
-      'guard.js': 'file_nestguard',
-      'module.js': 'file_nestmodule',
-      'module.ts': 'file_nestmodule',
-      'service.js': 'file_nestservice',
-      'service.spec.js': 'file_nestservicetest',
-      'service.ts': 'file_nestservice',
-      'service.spec.ts': 'file_nestservicetest',
-      'pipe.ts': 'file_nestpipe',
-      'pipe.js': 'file_nestpipe'
+      "controller.ts": "file_nestcontroller",
+      "controller.spec.ts": "file_nestcontrollertest",
+      "controller.js": "file_nestcontroller",
+      "controller.spec.js": "file_nestcontrollertest",
+      "guard.ts": "file_nestguard",
+      "guard.js": "file_nestguard",
+      "module.js": "file_nestmodule",
+      "module.ts": "file_nestmodule",
+      "service.js": "file_nestservice",
+      "service.spec.js": "file_nestservicetest",
+      "service.ts": "file_nestservice",
+      "service.spec.ts": "file_nestservicetest",
+      "pipe.ts": "file_nestpipe",
+      "pipe.js": "file_nestpipe",
     };
   }
 }
 
-function updateIconPath(type: 'angular' | 'nestjs') {
-  const themes = ['dark', 'light', 'auto'];
+function updateIconPath(type: "angular" | "nestjs") {
+  const themes = ["dark", "light", "auto"];
   const fileExtensions = getFileExtensions(type);
 
-  themes.forEach(theme => {
-    const configPath = path.join(__dirname, '..', `dist/2023/${theme}`, 'theme.json');
-    const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+  themes.forEach((theme) => {
+    const configPath = path.join(
+      __dirname,
+      "..",
+      `dist/2023/${theme}`,
+      "theme.json"
+    );
+    const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
 
     if (config.fileExtensions) {
-      Object.keys(fileExtensions).forEach(extension => {
+      Object.keys(fileExtensions).forEach((extension) => {
         config.fileExtensions[extension] = fileExtensions[extension];
       });
     }
